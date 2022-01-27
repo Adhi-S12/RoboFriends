f41f533731ed96734c7f280ad5b0d401020003b6
Adhi <adhipathipandiyan.s12@gmail.com>
2022-01-27 11:07:18 +0530

    Updated App Component with Functional component and used hooks for state management and running life cycle methods

--- src/App.js
+++ src/App.js
@@ -1,53 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import CardList from './components/CardList';
 import SearchBox from './components/SearchBox';
 import Scroll from './components/Scroll';
 import ErrorBoundary from './components/ErrorBoundary';
 
-class App extends Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchField: '',
-		};
-	}
+const App = () => {
+	const [ robots, setRobots ] = useState([]);
+	const [ searchField, setSearchField ] = useState('');
 
-	onSearchChange = (event) => {
-		this.setState({
-			searchField: event.target.value,
-		});
+	const fetchData = async () => {
+		const res = await fetch('https://jsonplaceholder.typicode.com/users');
+		const users = await res.json();
+		setRobots(users);
 	};
 
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then((res) => res.json())
-			.then((users) => this.setState({ robots: users }));
-	}
+	const onSearchChange = (e) => {
+		setSearchField(e.target.value);
+	};
 
-	render() {
-		const filteredRobots = this.state.robots.filter((robot) => {
-			return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
-		});
-		if (this.state.robots.length === 0) {
-			return <h1 className="tc">Loading...</h1>;
-		} else {
-			return (
-				<div style={{ textAlign: 'center' }}>
-					<h1 className="f1" style={{ marginTop: 0, paddingTop: '30px' }}>
-						RoboFriends
-					</h1>
-					<SearchBox searchChange={this.onSearchChange} />
-					<Scroll>
-						<ErrorBoundary>
-							<CardList robots={filteredRobots} />
-						</ErrorBoundary>
-					</Scroll>
-				</div>
-			);
-		}
+	useEffect(() => {
+		fetchData();
+	}, []);
+
+	const filteredRobots = robots.filter((robot) => {
+		return robot.name.toLowerCase().includes(searchField.toLowerCase());
+	});
+	if (robots.length === 0) {
+		return <h1 className="tc">Loading...</h1>;
+	} else {
+		return (
+			<div style={{ textAlign: 'center' }}>
+				<h1 className="f1" style={{ marginTop: 0, paddingTop: '30px' }}>
+					RoboFriends
+				</h1>
+				<SearchBox searchChange={onSearchChange} />
+				<Scroll>
+					<ErrorBoundary>
+						<CardList robots={filteredRobots} />
+					</ErrorBoundary>
+				</Scroll>
+			</div>
+		);
 	}
-}
+};
 
 export default App;
