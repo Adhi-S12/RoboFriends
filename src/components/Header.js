3c9d69398cc53a26a0d8921b223cac7733b69aa0
Adhi <adhipathipandiyan.s12@gmail.com>
2022-02-01 08:51:55 +0530

    centered header and gave it a font size

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderComponent = () => {
 	return (
-		<h1 className="f1" style={{ marginTop: 0, paddingTop: '30px' }}>
+		<h1 className="f1" style={{ marginTop: 0, paddingTop: '30px', fontSize: 40, textAlign: 'center' }}>
 			RoboFriends
 		</h1>
 	);
